e99c517ad42c057cf1c0362c4f305e782f3b9a7f
Thomas Wagener <thomas.wagener@fischantiquariat.de>
2025-08-15 11:55:28 +0200

    Corrected connection string Added mongodb dependencies

--- ch04/example-3/video-streaming/src/main.rs
+++ ch04/example-3/video-streaming/src/main.rs
@@ -1,10 +1,28 @@
-use axum::{Router, body::Body, extract::State, response::IntoResponse, routing::get};
-use std::env;
+use axum::{
+    Router,
+    body::Body,
+    extract::{Query, State},
+    response::IntoResponse,
+    routing::get,
+};
+use mongodb::bson::Uuid;
+use serde::Deserialize;
+use std::{env, str::FromStr};
+
+struct Item {
+    id: mongodb::bson::oid::ObjectId,
+}
+
+#[derive(Deserialize)]
+struct Video {
+    video_path: String,
+}
 
 #[derive(Clone)]
 struct AppState {
     video_storage_host: String,
     video_storage_port: String,
+    client: mongodb::Client,
 }
 
 #[tokio::main]
@@ -14,10 +32,23 @@ async fn main() {
         env::var("VIDEO_STORAGE_HOST").expect("VIDEO_STORAGE_HOST environment variable not set");
     let video_storage_port =
         env::var("VIDEO_STORAGE_PORT").expect("VIDEO_STORAGE_PORT environment variable not set");
+    let db_host = env::var("DBHOST").expect("DBHOST environment variable not set");
+    let db_name = env::var("DBNAME").expect("DBNAME environment variable not set");
 
+    let connection_string = format!("mongodb://{db_host}/{db_name}");
+    let mut client_options = mongodb::options::ClientOptions::parse(connection_string)
+        .await
+        .expect("Can not create connection options");
+    let server_api = mongodb::options::ServerApi::builder()
+        .version(mongodb::options::ServerApiVersion::V1)
+        .build();
+    client_options.server_api = Some(server_api);
+    let client = mongodb::Client::with_options(client_options).expect("Can not create clients");
+    client.database("videos");
     let app_state = AppState {
         video_storage_host,
         video_storage_port,
+        client,
     };
     let app = app(app_state);
 
@@ -38,7 +69,13 @@ fn app(state: AppState) -> Router {
         .with_state(state)
 }
 
-async fn get_video(State(state): State<AppState>) -> impl IntoResponse {
+async fn get_video(State(state): State<AppState>, Query(id): Query<String>) -> impl IntoResponse {
+    let video_id = mongodb::bson::oid::ObjectId::from_str(&id);
+    let videos = state
+        .client
+        .database("video-streaming")
+        .collection::<Video>("videos");
+    let video_record = videos.find_one(Item { id: video_id });
     let video_storage_host = &state.video_storage_host;
     let video_storage_port = &state.video_storage_port;
     let forward_response = reqwest::Client::new()
